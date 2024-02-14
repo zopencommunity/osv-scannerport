ebe264e3eb8014f7949b5d1483a44254da4381ad
Igor Todorovski <itodorov@ca.ibm.com>
2024-02-14 12:11:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+osv-scanner
+
+Vulnerability scanner written in Go which uses the data provided by https://osv.dev
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/osv-scannerport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/osv-scannerport.git'), string(name: 'PORT_DESCRIPTION', value: 'Vulnerability scanner written in Go which uses the data provided by https://osv.dev' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
