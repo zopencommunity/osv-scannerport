1636e52bbfefc619c32a70278aaccf490c8aeeaa
Igor Todorovski <itodorov@ca.ibm.com>
2024-02-14 12:39:20 -0500

    Add go 1215

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/osv-scannerport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/osv-scannerport.git'), string(name: 'PORT_DESCRIPTION', value: 'Vulnerability scanner written in Go which uses the data provided by https://osv.dev' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/osv-scannerport.git'), string(name: 'PORT_DESCRIPTION', value: 'Vulnerability scanner written in Go which uses the data provided by https://osv.dev' ), string(name: 'BUILD_LINE', value: 'STABLE'),  string(name: 'NODE_LABEL', value: "go_1215" ) ]
   }
 }
