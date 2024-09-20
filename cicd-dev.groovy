5363008c5a8c7ba7ee74d75f2ad26c26364df9b0
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:33 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/osv-scannerport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/osv-scannerport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/osv-scannerport.git'), string(name: 'PORT_DESCRIPTION', value: 'Vulnerability scanner written in Go which uses the data provided by https://osv.dev' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/osv-scannerport.git'), string(name: 'PORT_DESCRIPTION', value: 'Vulnerability scanner written in Go which uses the data provided by https://osv.dev' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
